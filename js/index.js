628d5f7b1042fd9dfd938b933ffa990cda10a2a4
Nayeli <nayeli585052@gmail.com>
2024-05-28 20:48:56 -0500

    Added bootstrap, added an image, linked all of the files togethe

--- js/index.js
+++ js/index.js
@@ -10,18 +10,21 @@
  document.body.appendChild(footer);
 
  //This is for the skills section
- let skills =["Javascript","HTML","CSS","Github"];
+ let skills =["Javascript","HTML","CSS","Git","Github"];
  let skillsSection = document.getElementById("skills");
  let skillsList = document.createElement("ul");
  skillsSection.appendChild(skillsList);
 
+
  for (let skill of skills) {
     let skillItem = document.createElement("li");
+    skillItem.setAttribute("class", "flex-item");
+    skillItem.setAttribute("id", "skillItem");
     skillItem.innerText = skill;
     skillsList.appendChild(skillItem);
  }
 
- // This is tthe message section
+ // This is the message section
 var messageForm = document.forms.leave_message;
 
 messageForm.addEventListener('submit', function(event) {
@@ -36,25 +39,35 @@ messageForm.addEventListener('submit', function(event) {
  console.log('Email:', userEmail);
  console.log('Message', userMessage);
 
-var messageSection = document.getElementById('messages');
+var messageSection = document.getElementById('messages2');
 var messageList = messageSection.querySelector("ul");
 var newMessage = document.createElement("li");
 newMessage.innerHTML = '<a href="mailto:' + userEmail + '">' + userName + '</a>: <span>' + userMessage + '</span>';
 
-var removeButton = document.createElement("button");
-removeButton.innerText = "remove";
-removeButton.type = "button";
 
-removeButton.addEventListener('click', function() {
-    var entry = removeButton.parentNode;
-    entry.remove();
-});
+        // This creates a remove button for the message
+        const removeBtn = document.createElement("button");
+        removeBtn.innerText = "delete";
+        removeBtn.type = "button";
+        removeBtn.addEventListener("click", function() {
+            const entry = removeBtn.parentNode;
+            entry.remove();
+            if (messageList.children.length === 0) {
+                messageSection.style.display = "none";
+            }
+        });
 
-newMessage.appendChild(removeButton);
+        // This is to add the remove button to the new message
+        newMessage.appendChild(removeButton);
 
-messageList.appendChild(newMessage);
+        // This is to add the new message to the message list
+        messageList.appendChild(newMessage);
 
- messageForm.reset();
+        // This is to clear the form after submitting
+        messageForm.reset();
+
+        // This displays the messages section
+        messageSection.style.display = "block";
 
  return false;
 });
@@ -69,6 +82,7 @@ document.addEventListener('DOMContentLoaded', function() {
         return response.json();
     })
     .then(data =>  {
+        
         // Get the projects section
         console.log(data);
         let projectsSection = document.getElementById('projects');
@@ -80,6 +94,8 @@ document.addEventListener('DOMContentLoaded', function() {
         // Loop through repositories and create list items for each project
         data.forEach(repo => {
             var project = document.createElement("li");
+            //added class to list items
+            project.setAttribute("class", "flex-item")
             project.innerText = repo.name;
             projectsList.appendChild(project);
         });
